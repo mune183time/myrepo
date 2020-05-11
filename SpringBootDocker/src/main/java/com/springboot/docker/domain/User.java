4aa07b4eeebd3e75fe1e3fd477bb4abbcf6108df
Administrator <mune183com@gmail.com>
2020-05-11 11:01:10 +0900

    aa

--- SpringBootDocker/src/main/java/com/springboot/docker/domain/User.java
+++ SpringBootDocker/src/main/java/com/springboot/docker/domain/User.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "USER")
+@Entity(name = "USER_TBL")
 public class User {
 
     @Id
