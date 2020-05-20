e132c1f5044fbc81e7d5c03a840be9c4a6f40988
Administrator <mune183com@gmail.com>
2020-05-20 09:59:04 +0900

    a

--- SpringBootDocker/src/main/java/com/springboot/docker/config/WebSecurityConfig.java
+++ SpringBootDocker/src/main/java/com/springboot/docker/config/WebSecurityConfig.java
@@ -1,18 +1,15 @@
 package com.springboot.docker.config;
 
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+public class WebSecurityConfig {//extends WebSecurityConfigurerAdapter {
 
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(
-                "/images/**",
-                "/css/**",
-                "/javascript/**",
-                "/webjars/**");
-    }
+    //    @Override
+    //    public void configure(WebSecurity web) throws Exception {
+    //        web.ignoring().antMatchers(
+    //                "/images/**",
+    //                "/css/**",
+    //                "/javascript/**",
+    //                "/webjars/**");
+    //    }
 
     //    @Override
     //    public void configure(HttpSecurity http) throws Exception {
