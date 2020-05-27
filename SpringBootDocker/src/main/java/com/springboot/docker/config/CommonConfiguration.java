23fd935277dd9c44681f9e55ff881efe47bed75e
Administrator <mune183com@gmail.com>
2020-05-27 14:32:43 +0900

    aa

--- SpringBootDocker/src/main/java/com/springboot/docker/config/CommonConfiguration.java
+++ SpringBootDocker/src/main/java/com/springboot/docker/config/CommonConfiguration.java
@@ -1,8 +1,10 @@
 package com.springboot.docker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
@@ -11,9 +13,14 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @Configuration
 public class CommonConfiguration {
 
+    private @Value("${spring.redis.host}") String redisHost;
+    private @Value("${spring.redis.port}") int redisPort;
+
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(
+                new RedisStandaloneConfiguration(redisHost, redisPort));
+        return lettuceConnectionFactory;
     }
 
 }
